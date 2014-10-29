24bdb18dab9b352be91d5ac4e64cd6709e3e929a
annaksnorra <annaksnorra@gmail.com>
2014-10-29 00:31:29 +0000

    Bætti við gradle í branchið annak og bætti við void fallinu enterMove

---
+++ src/test/java/is/fantastic7/tictactoe/TicTacToeTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import java.util.Scanner;
+import static java.lang.System.out;
+
+
+public class TicTacToeTest {
+
+	private TicTacToe t;
+
+	@Before
+	public void setUp() {
+		t = new TicTacToe();
+	}
+
+	@Test
+	public void TestInitialGameBoard()
+	{
+		t.initialGameBoard();
+		assertEquals(t.PLAYS, t.state);
+		assertEquals(t.EX, t.player);
+	}
+	
+	/*
+	@Test
+	public void TestEnterMove()
+	{
+		//int wrong = 3;
+		Scanner readin = new Scanner(System.in);
+		assertEquals("Player X make your move, choose row(1-3) and column(1-3)", t.enterMove());
+
+		//while(wrong >=4 || wrong <= 0) {
+		//	assertEquals("Illegal move! Try again...", t.enterMove(0));
+		//}
+	}
+	*/
+	
+}
