3479c06454176d41261645de62fb896be38c7b36
annaksnorra <annaksnorra@gmail.com>
2014-10-30 21:58:51 +0000

    testa fyrir playerX og hvort að það komi inn 3x3 fylki.

--- src/main/java/is/fantastic7/tictactoe/TicTacToe.java
+++ src/main/java/is/fantastic7/tictactoe/TicTacToe.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
 import static java.lang.System.out;
 
-public class TicTacToe 
-{
-	
-
+public class TicTacToe {
+	public char[][] board;
+	public char playerX;
+	public TicTacToe() {
+		board = new char [3][3];
+		playerX = 'x';
+		newBoard();
+	}
+	public void newBoard() {
+		for(int rows = 0; rows < 3; rows++) {
+			for(int cols = 0; cols < 3; cols++) {
+				board[rows][cols] = '-';
+			}
+		}
+	}
 }
-
-
-
 
 
 
--- src/test/java/is/fantastic7/tictactoe/TicTacToeTest.java
+++ src/test/java/is/fantastic7/tictactoe/TicTacToeTest.java
@@ -7,5 +7,25 @@ import static java.lang.System.out;
 
 public class TicTacToeTest 
 {
-	
+	private TicTacToe t;
+
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.fantastic7.tictactoe.TicTacToeTest");
+	}
+
+	@Before
+	public void setup() {
+		t = new TicTacToe();
+	}
+
+	@Test
+	public void testTicTacToeConstructor() {
+		assertEquals('x', t.playerX);
+		for(int i = 0; i < 3; i++) {
+			assertEquals(3, t.board[i].length);
+		}
+		
+	}
+
+
 }
