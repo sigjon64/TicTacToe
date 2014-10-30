24ace566ccb43c2a5245b4979a00da3a3890e806
GudrunInga <gudrunib09>
2014-10-30 13:18:31 +0000

    build.gradle fixed and test/main erased

--- src/main/java/is/fantastic7/tictactoe/TicTacToe.java
+++ src/main/java/is/fantastic7/tictactoe/TicTacToe.java
@@ -3,56 +3,11 @@ import static java.lang.System.out;
 
 public class TicTacToe {
 
-	public static final int ROW = 2; //
-	public static final int COL = 2; // 
-
-	public static final int PLAYS = 0;
-	
-	public static final int EMPTY = 0; // " " 
-	public static final int EX = 1; // "X"
-	public static int player;
-	public static int state;
-	public static int [][] playBoard = new int [ROW][COL];
-	public static int row;
-	public static int col;
-	public static int [][] playBoard = new int [ROW][COL];
-
-	public static Scanner readin = new Scanner(System.in);
-		
-
-	public static void initialGameBoard() {
-		for (int row = 0; row < ROW; row++) {
-			for (int col = 0; col < COL; col++) {
-				playBoard[row][col] = EMPTY;
-			}
-		}
-		state = PLAYS;
-		player = EX;
-	}
-
-	public static void enterMove(int theDoer) {
-		boolean leagalMove = false;
-		do {
-			if (theDoer == EX) {
-				System.out.println("Player X make your move, choose row(1-3) and column(1-3)");
-			}
-			else {
-				System.out.println("Player O make your move, choose row(1-3) and column(1-3)");
-			}
-			int inrow = readin.nextInt() - 1;
-			int incol = readin.nextInt() - 1;
-
-			if(inrow >= 0 && inrow < ROW && incol >= 0 && incol < COL && playBoard[inrow][incol] == EMPTY) {
-				row = inrow;
-				col = incol;
-				playBoard[row][col] = theDoer;
-				leagalMove = true;
-			}
-			else {
-				System.out.println("Illegal move! Try again...");
-			}
-
-		}
-		while(leagalMove = true);
-	}
 }
+
+
+
+
+
+
+
--- src/test/java/is/fantastic7/tictactoe/TicTacToeTest.java
+++ src/test/java/is/fantastic7/tictactoe/TicTacToeTest.java
@@ -5,36 +5,8 @@ import java.util.Scanner;
 import static java.lang.System.out;
 
 
-public class TicTacToeTest {
+public class TicTacToeTest 
+{
 
-	private TicTacToe t;
-
-	@Before
-	public void setUp() {
-		t = new TicTacToe();
-	}
-
-	@Test
-	public void TestInitialGameBoard()
-	{
-		t.initialGameBoard();
-		assertEquals(t.PLAYS, t.state);
-		assertEquals(t.EX, t.player);
-	}
-}
-	
-	/*
-	@Test
-	public void TestEnterMove()
-	{
-		//int wrong = 3;
-		Scanner readin = new Scanner(System.in);
-		assertEquals("Player X make your move, choose row(1-3) and column(1-3)", t.enterMove());
-
-		//while(wrong >=4 || wrong <= 0) {
-		//	assertEquals("Illegal move! Try again...", t.enterMove(0));
-		//}
-	}
-	*/
 	
 }
